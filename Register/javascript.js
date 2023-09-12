489a79ee0682f851b45ba5f7d5f92452e923d269
PaswanAlka <124689897+PaswanAlka@users.noreply.github.com>
2023-09-12 22:18:51 +0530

    Add files via upload

---
+++ Register/javascript.js
@@ -0,0 +1,15 @@
+function validateForm() {
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match!");
+        return false;
+    }
+
+    // Add additional validation checks here if needed
+    
+    return true;
+}
